8edcd13ca62ad35654da08cf874198cda4a108b3
Sudikoff Lab iMac <imac@maclab-210-45.lab.cs.dartmouth.edu>
2019-09-30 10:00:10 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Sudikoff Lab iMac on 9/30/19.
+//  Copyright © 2019 Rafa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
