8b105f41600f04a601a22ab9dc2cda6b9457b766
Rafael Brantley <rafael.brantley@gmail.com>
2019-10-01 11:59:09 -0400

    Implemented code linked to button.

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,13 +8,50 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+// the comma "adopts" the UITextField Delegate "protocol"
+class ViewController: UIViewController, UITextFieldDelegate {
+    
+    //MARK: Properties
 
+    @IBOutlet weak var nameTextField: UITextField!
+
+    @IBOutlet weak var mealNameLabel: UILabel!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // "self" refers to ViewController
+        // So this class will now set itself as the delegate of the nameTextField element
+        nameTextField.delegate = self
     }
 
+    
+    
+    //MARK: UITextFieldDelegate
+    
+    // Gets called when the user taps out of the text-box
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
 
+        // Hide the keyboard
+        textField.resignFirstResponder()
+        
+        return true
+    }
+    
+    // Gets called when the text field resigns "first responder" status
+    // recall first responder status directs input from the keyboard into the box with the status
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        
+        mealNameLabel.text = textField.text
+    }
+    
+
+    //MARK: Actions
+    @IBAction func setDefaultLabelText(_ sender: UIButton) {
+        
+        mealNameLabel.text = "Default Text"
+    }
 }
 
